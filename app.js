a4d1a658792e93128f0495c339f707de1a0836f5
David Alvarado <davidalvarado@Davids-MacBook-Pro.local>
2017-12-11 10:28:27 -0700

    Connect MySQL and API

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var request = require('request');
+var mysql = require('mysql');
+var MysqlJson = require('mysql-json');
 var index = require('./routes/index');
-
 
 var app = express();
 
@@ -14,11 +12,49 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// MySQL connection
+var connection = new MysqlJson({
+  host:'127.0.0.1',
+  user:'root',
+  password:'root',
+  database: 'candidate_alvaradod'
+});
+
+// Check connection
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected!');
+});
+
+
+var options = { method: 'GET',
+  url: 'https://api.themoviedb.org/3/discover/movie',
+  qs:
+   {
+     primary_release_year: '2015',
+     genres: 18,
+     'certification.lte': 'r',
+     include_video: 'false',
+     include_adult: 'false',
+     sort_by: 'popularity.desc',
+     api_key: 'b7766f055f7482d017a2fbf7f1603ca3' },
+     page: '1',
+     page: '2',
+     page: '3',
+     page: '4',
+     page: '5',
+  body: '{}' };
+
+request(options, function (error, response, body) {
+  if (error) throw new Error(error);
+
+  console.log(body);
+});
+
 
 app.use('/', index);
 
