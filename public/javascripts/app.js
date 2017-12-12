6a2fa15ab038221ca41b63ab914fda0051978f18
David Alvarado <davidalvarado@Davids-MacBook-Pro.local>
2017-12-11 19:49:19 -0700

    Refactor UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,19 @@
 var app = angular.module('myApp',[]);
 app.controller('displayController', function($scope, $http) {
     $scope.data = [];
+
     var request = $http.get('/data');
     request.success(function(data) {
         $scope.data = data;
         console.log(data);
     });
+
     request.error(function(data){
         console.log('Error: ' + data);
     });
+
     $scope.orderByMe = function(x) {
       $scope.myOrder = x;
     }
+
 });
